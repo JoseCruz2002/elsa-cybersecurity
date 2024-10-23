f59457295604c470ea71e130a808c435cf907db0
José Cruz <jose.melo.neves.cruz@tecnico.ulisboa.pt>
2024-10-23 19:03:57 +0200

    fix to comparison.py - use the proper labels for the test sets

--- track_1/comparison.py
+++ track_1/comparison.py
@@ -2,63 +2,43 @@ import json
 import os
 from models.utils import *
 
-def find_string_in_file(document, string):
-    '''
-    This function performs a find as in Ctrl+f on a file.
-    file_path: document already loaded as a dictionary.
-    string: string to file in the file
-    '''
-    for key in document:
-        if key == string:
-            return True
-    return False
-
-def is_pred_correct(pred_label, document, string):
-    is_malware = find_string_in_file(document, string)
-    return (pred_label == 0 and not is_malware) or (pred_label == 1 and is_malware)
-
 if __name__ == "__main__":
 
     submissions_path = os.path.join(os.path.dirname(__file__), "submissions/")
     comparison_path = os.path.join(os.path.dirname(__file__), "results/comparison.json")
     results_path = os.path.join(os.path.dirname(__file__), "results/results.json")
 
-    base_path = os.path.join(os.path.dirname(__file__))
-    names_proposed_path = os.path.join(base_path, "../data/names_proposed.json")
-    types_proposed_path = os.path.join(base_path, "../data/types_proposed.json")
+    base_path = os.path.join(os.path.dirname(__file__)) 
+    malware_apks_path = os.path.join(base_path, "../data/test_set_adv.zip")
+    mal_apks_shas = load_sha256_list(malware_apks_path)
 
-    with open(names_proposed_path, "r") as na:
-        with open(types_proposed_path, "r") as ty:
-            na_doc = json.load(na)
-            ty_doc = json.load(ty)
-            results = {}
-            accuracies = {}
-            avg_scores = {}
-            results_acc_scores = {}
-            counts = {}
-            for filename in os.listdir(submissions_path):
-                if filename == ".gitkeep":
-                    continue
-                accuracies[filename.split(".")[0]] = 0
-                avg_scores[filename.split(".")[0]] = 0
-                count = 0
-                with open(os.path.join(submissions_path, filename), 'r') as f:
-                    print("________________________", filename)
-                    list = json.load(f)
-                    for test in list:
-                        for sha256 in test:
-                            if sha256 in results:
-                                results[sha256] += test[sha256]
-                            else:
-                                results[sha256] = test[sha256]
-                            accuracies[filename.split(".")[0]] += 1 \
-                                    if is_pred_correct(test[sha256][0], na_doc, sha256) or is_pred_correct(test[sha256][0], ty_doc, sha256) else 0
-                            avg_scores[filename.split(".")[0]] += test[sha256][1]
-                            count += 1
-                            print(f"count = {count}")
-                counts[filename.split(".")[0]] = count
-                results_acc_scores[filename.split(".")[0]] = [accuracies[filename.split(".")[0]] / count]
-                results_acc_scores[filename.split(".")[0]] += [avg_scores[filename.split(".")[0]] / count]
+    results = {}
+    accuracies = {}
+    avg_scores = {}
+    results_acc_scores = {}
+    counts = {}
+    for filename in os.listdir(submissions_path):
+        if filename == ".gitkeep":
+            continue
+        accuracies[filename.split(".")[0]] = 0
+        avg_scores[filename.split(".")[0]] = 0
+        count = 0
+        with open(os.path.join(submissions_path, filename), 'r') as f:
+            print("________________________", filename)
+            list = json.load(f)
+            for test in list:
+                for sha256 in test:
+                    if sha256 in results:
+                        results[sha256] += test[sha256]
+                    else:
+                        results[sha256] = test[sha256]
+                    accuracies[filename.split(".")[0]] += 1 \
+                            if (sha256 in mal_apks_shas and test[sha256][0] == 1) or (sha256 not in mal_apks_shas and test[sha256][0] == 0) else 0
+                    count += 1
+                    print(f"count = {count}")
+        counts[filename.split(".")[0]] = count
+        results_acc_scores[filename.split(".")[0]] = [accuracies[filename.split(".")[0]] / count]
+        results_acc_scores[filename.split(".")[0]] += [avg_scores[filename.split(".")[0]] / count]
     
     for model in counts:
         print(f"count for model {model}: {counts[model]}")
